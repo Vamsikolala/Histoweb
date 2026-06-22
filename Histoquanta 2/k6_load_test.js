4fb01c1d9780f01f9f1e0fc2159ee6dbd257addc
Vamsikolala <vamsikolala@users.noreply.github.com>
2026-06-22 14:56:43 +0530

    fix: switch to lightweight text summary to avoid k6 compiler crash

--- Histoquanta 2/k6_load_test.js
+++ Histoquanta 2/k6_load_test.js
@@ -1,6 +1,5 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 export const options = {
   vus: 300, // 300 virtual users
@@ -27,7 +26,7 @@ export default function () {
 
 export function handleSummary(data) {
   return {
-    "k6_report.html": htmlReport(data),
+    "k6_report.txt": textSummary(data, { indent: " ", enableColors: false }),
     stdout: textSummary(data, { indent: " ", enableColors: true }),
   };
 }
